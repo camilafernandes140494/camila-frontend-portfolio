de6065ea329c5aa002e85c267338be1bf282edec
Camila <camila.silva@nappsolutions.com>
2024-10-07 15:52:56 -0300

    fix: adjust layout

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,5 +1,7 @@
-import { Avatar, Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import camilaImage from "/src/assets/images/camila.jpg";
+import molde from "/src/assets/svg/molde.svg";
 
 export const AboutMe = () => {
   const { t } = useTranslation();
@@ -33,12 +35,20 @@ export const AboutMe = () => {
             gap: "1rem",
           }}
         >
-          <Typography variant="h4">{t("hello")}</Typography>
-          <Typography variant="h2" sx={{ fontFamily: "Sacramento" }}>
+          <Typography variant="h4" textAlign={{ xs: "center", md: "left" }}>
+            {t("hello")}
+          </Typography>
+          <Typography
+            variant="h2"
+            sx={{ fontFamily: "Sacramento" }}
+            textAlign={{ xs: "center", md: "left" }}
+          >
             {"Camila Fernandes"}
           </Typography>
 
-          <Typography variant="h5">{t("frontend_developer")}</Typography>
+          <Typography variant="h5" textAlign={{ xs: "center", md: "left" }}>
+            {t("frontend_developer")}
+          </Typography>
           <Button
             variant="contained"
             sx={{
@@ -50,7 +60,43 @@ export const AboutMe = () => {
           </Button>
         </Box>
 
-        <Avatar sx={{ width: "10rem", height: "10rem" }}>CF</Avatar>
+        <Box
+          sx={{
+            position: "relative",
+            width: "25rem",
+            height: "25rem",
+          }}
+        >
+          <img src={molde} alt="Background SVG" width="100%" height="100%" />
+          <svg
+            width="80%"
+            height="80%"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <defs>
+              <mask id="svg-mask">
+                <image
+                  href={molde}
+                  width="100%"
+                  height="100%"
+                  mask="url(#svg-mask)"
+                />
+              </mask>
+            </defs>
+            <image
+              href={camilaImage}
+              width="100%"
+              height="100%"
+              mask="url(#svg-mask)"
+            />
+          </svg>
+        </Box>
       </Box>
 
       <Box
--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -25,17 +25,56 @@ export const Skills = () => {
           flexWrap: "wrap",
         }}
       >
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
-        <SkillCard icon="mdi:language-html5" skill="HTML 5" />
+        <SkillCard
+          icon="catppuccin:typescript-react"
+          skill="React"
+          ratingValue={5}
+        />
+        <SkillCard
+          icon="catppuccin:typescript"
+          skill="TypeScript"
+          ratingValue={5}
+        />
+        <SkillCard
+          icon="catppuccin:javascript"
+          skill="JavaScript"
+          ratingValue={5}
+        />
+        <SkillCard
+          icon="catppuccin:typescript-react"
+          skill="React Native"
+          ratingValue={4.5}
+        />
+        <SkillCard
+          icon="devicon:materialui"
+          skill="Material UI"
+          ratingValue={5}
+        />
+        <SkillCard icon="catppuccin:next" skill="Next.js" ratingValue={3.5} />
+        <SkillCard
+          icon="logos:react-query-icon"
+          skill="React Query"
+          ratingValue={5}
+        />
+        <SkillCard icon="catppuccin:html" skill="HTML 5" ratingValue={5} />
+        <SkillCard
+          icon="catppuccin:tailwind"
+          skill="Tailwind CSS"
+          ratingValue={3.5}
+        />
+        <SkillCard icon="catppuccin:python" skill="Python" ratingValue={3.5} />
+        <SkillCard icon="catppuccin:figma" skill="Figma" ratingValue={3.5} />
+        <SkillCard
+          icon="catppuccin:storybook-vue"
+          skill="Storybook"
+          ratingValue={3.5}
+        />
+        <SkillCard icon="catppuccin:jest" skill="Jest" ratingValue={2.5} />
+        <SkillCard
+          icon="catppuccin:cypress"
+          skill="Cypress"
+          ratingValue={2.5}
+        />
       </Box>
     </Box>
   );
